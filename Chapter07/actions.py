3351c59d68d0f51004e524cb9c4ce33e143c8190
Xiaoquan Kong <u1mail2me@gmail.com>
2021-08-26 01:15:56 +0800

    update for chapter07

--- Chapter07/actions.py
+++ Chapter07/actions.py
@@ -3,7 +3,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Tracker, Action
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction, REQUESTED_SLOT
+from rasa_sdk.forms import FormAction
 
 
 class TicketFormAction(FormAction):
@@ -11,12 +11,56 @@ class TicketFormAction(FormAction):
         return "action_ticket_form_submit"
 
     def run(
-        self, dispatch: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+            self, dispatch: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]
     ) -> List[Dict]:
-        # don't using template alone,
-        # since the system tracker is not updated yet when render the template,
-        # using current tracker instead
-        dispatch.utter_message(template="utter_ask_confirm", **tracker.slots)
+        # print out all entities
+        dispatch.utter_message("Entities: " + tracker.latest_message[
+            'entities'] + "\n" + "Slots: " + tracker.slots)
+        return []
+
+
+class TicketQueryAction(Action):
+    def name(self) -> Text:
+        return "action_ticket_response"
+
+    def run(self, dispatch, tracker, domain):
+        entities = []
+        for entity in tracker.latest_message['entities']:
+            entities.append(
+                {"entity": entity["entity"], "value": entity["value"],
+                 "role": entity.get("role")})
+        msg = str(entities) + "\n"
+
+        dispatch.utter_message(msg)
+        return []
+
+
+class DrinkQueryAction(Action):
+    def name(self) -> Text:
+        return "action_drink_response"
+
+    def run(self, dispatch, tracker,domain):
+        # print out entities by group
+        msg = ""
+
+        entities = []
+        for entity in tracker.latest_message['entities']:
+            if entity.get("group") == "1":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"],
+                     "group": entity["group"]})
+        msg += "group #1: " + str(entities) + "\n"
+
+        entities = []
+        for entity in tracker.latest_message['entities']:
+            if entity.get("group") == "2":
+                entities.append(
+                    {"entity": entity["entity"], "value": entity["value"],
+                     "group": entity["group"]})
+        msg += "group #2: " + str(entities)
+
+        dispatch.utter_message(msg)
         return []
 
 
@@ -25,12 +69,11 @@ class ActionBuyTicket(Action):
         return "action_buy_ticket"
 
     def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-
         arrive = tracker.get_slot("city_arrive")
 
         api_succeed = arrive == "北京"
---
+++ Chapter07/tests.md
@@ -0,0 +1,2 @@
+One large cup of Cola . Another one is medium cup of Fanta .The former one with ice, and the latter one without ice
+Help me order a ticket from Berlin to Paris tomorrow
